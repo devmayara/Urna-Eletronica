276e3b8c83e653f8bf1f79ac1e1f3ab1d108248b
Mayara Silva <devmayara11@gmail.com>
2022-06-27 09:13:58 -0300

    Preenchendo Interface com números

--- js/script.js
+++ js/script.js
@@ -6,10 +6,49 @@ let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
-let 
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+    numero = '';
+    votoBranco = false;
+
+    for(let i=0;i<etapa.numeros;i++) {
+        if(i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+    alert('Finalizou!')
+}
 
 function clicou(n) {
-    alert('Clicou em: '+n);
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+    }
 }
 
 function branco() {
@@ -23,3 +62,5 @@ function corrige() {
 function confirma() {
     alert('Clicou em: CONFIRMA!');    
 }
+
+comecarEtapa();
