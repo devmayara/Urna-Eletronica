737489980f1869dfa24595518399897ab7c95d41
Mayara Silva <devmayara11@gmail.com>
2022-06-27 08:31:39 -0300

    update

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+
+let 
+
+function clicou(n) {
+    alert('Clicou em: '+n);
+}
+
+function branco() {
+    alert('Clicou em: BRANCO!');    
+}
+
+function corrige() {
+    alert('Clicou em: CORRIGE!');    
+}
+
+function confirma() {
+    alert('Clicou em: CONFIRMA!');    
+}
